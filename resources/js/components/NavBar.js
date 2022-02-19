baee2bdceb1db2dddae00b43cf08f664939fed03
Angelgomz <angelgomz14@gmail.com>
2022-02-19 01:41:32 -0300

    Ordenando navbar

--- resources/js/components/NavBar.js
+++ resources/js/components/NavBar.js
@@ -3,14 +3,15 @@ import { Nav } from 'react-bootstrap';
 import { Navbar } from 'react-bootstrap';
 import { Container } from 'react-bootstrap';
 import '../../../public/css/styles.css';
-
 function Navegacion() {
+ 
     return (
       <div>
        <Navbar>
         <Container>
           <Navbar.Brand href="/home">
                      <p> My Spotify App </p>
+                   
           </Navbar.Brand>
         </Container>
         </Navbar>
